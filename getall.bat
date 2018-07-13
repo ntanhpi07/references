2d0fc93584247d355bfc91593e13c2ca5558cd86
ntanhpi07 <32693504+ntanhpi07@users.noreply.github.com>
2018-07-13 15:17:55 -0400

    Add files via upload
    ok

---
+++ getall.bat
@@ -0,0 +1,10 @@
+wstart -w -d ADConstructInputFiles ADConstructInputFiles\GetSomeADObjects.exe
+wstart -d ADGroups ADGroups\GetADGroups.exe
+wstart -d ADUsers\GetUsers1 ADUsers\GetUsers1\GetADUsers.exe
+wstart -d ADUsers\GetUsers2 ADUsers\GetUsers2\GetADUsers.exe
+wstart -d ADUsers\GetUsers3 ADUsers\GetUsers3\GetADUsers.exe
+wstart -d ADUsers\GetUsers4 ADUsers\GetUsers4\GetADUsers.exe
+wstart -d ADUsers\GetAssignments1 ADUsers\GetAssignments1\GetADGroupAssignments.exe
+wstart -d ADUsers\GetAssignments2 ADUsers\GetAssignments2\GetADGroupAssignments.exe
+wstart -d ADUsers\GetAssignments3 ADUsers\GetAssignments3\GetADGroupAssignments.exe
+wstart -d ADUsers\GetAssignments4 ADUsers\GetAssignments4\GetADGroupAssignments.exe
---
+++ getall2.bat
@@ -0,0 +1,10 @@
+start /WAIT /D ADConstructInputFiles ADConstructInputFiles\GetSomeADObjects.exe
+start /D ADGroups ADGroups\GetADGroups.exe
+start /D ADUsers\GetUsers1 ADUsers\GetUsers1\GetADUsers.exe
+start /D ADUsers\GetUsers2 ADUsers\GetUsers2\GetADUsers.exe
+start /D ADUsers\GetUsers3 ADUsers\GetUsers3\GetADUsers.exe
+start /D ADUsers\GetUsers4 ADUsers\GetUsers4\GetADUsers.exe
+start /D ADUsers\GetAssignments1 ADUsers\GetAssignments1\GetADGroupAssignments.exe
+start /D ADUsers\GetAssignments2 ADUsers\GetAssignments2\GetADGroupAssignments.exe
+start /D ADUsers\GetAssignments3 ADUsers\GetAssignments3\GetADGroupAssignments.exe
+start /D ADUsers\GetAssignments4 ADUsers\GetAssignments4\GetADGroupAssignments.exe
