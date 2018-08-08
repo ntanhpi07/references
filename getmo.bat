b2b6b9754793797a1dd9b90598a0dcb2059917fb
ntanhpi07 <32693504+ntanhpi07@users.noreply.github.com>
2018-08-08 11:20:18 -0400

    Delete getmo.bat
    delete

--- getmo.bat
+++
@@ -1,8 +0,0 @@
-del ADUsers\GetUsers1\UserTracker.txt
-del ADUsers\GetUsers2\UserTracker.txt
-del ADUsers\GetUsers3\UserTracker.txt
-del ADUsers\GetUsers4\UserTracker.txt
-wstart -d ADUsers\GetUsers1 ADUsers\GetUsers1\GetADManagedObjects.exe
-wstart -d ADUsers\GetUsers2 ADUsers\GetUsers2\GetADManagedObjects.exe
-wstart -d ADUsers\GetUsers3 ADUsers\GetUsers3\GetADManagedObjects.exe
-wstart -d ADUsers\GetUsers4 ADUsers\GetUsers4\GetADManagedObjects.exe
